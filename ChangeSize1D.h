0a206ebc6bb5dc6ecc53cfb4f2b874f6bceec805
staronthemaze <skisthinking@gmail.com>
2014-05-20 11:15:55 +0800

    Create ChangeSize1D.h

---
+++ ChangeSize1D.h
@@ -0,0 +1,23 @@
+template<class T>
+void ChangeSize1D(T* &a,int n,int ToSize)
+	{
+	T* temp = new T[ToSize];
+	if (n>=ToSize)//缩小数组尺寸
+	{
+		for (int i = 0; i < ToSize; i++)
+		{
+			temp[i] = a[i];
+		}
+		delete [] a;
+		a = temp;
+	}
+	else//扩大数组尺寸
+	{
+		for (int i = 0; i < n; i++)
+		{
+			temp[i] = a[i];
+		}
+		delete [] a;
+		a = temp;
+	}
+}
